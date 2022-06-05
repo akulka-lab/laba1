523fe6240553f2a64157485af4d98fdd9684e285
akulka-lab <74815124+akulka-lab@users.noreply.github.com>
2022-06-05 18:07:39 +0300

    Update StringPro.c

--- StringPro.c
+++ StringPro.c
@@ -3,6 +3,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+void my_memcpy(char* newstr, char* str, int len)
+{
+    char* s1 = newstr;
+    char* s2 = str;
+
+    for (int i = 0; i < len; i++)
+    {
+        *(s1++) = *(s2++);
+    }
+}
 
 
 StringPro* initialize()
@@ -13,28 +23,32 @@ StringPro* initialize()
     StrPro->ptr = NULL;
 }
 
-void* KeyboardInput(void* str, int * error)
+void enteer()
 {
-    StringPro* StrPro = (StringPro*) str;
     printf(
         " _______________________________________________________________________  \n"
         "|                                                                       | \n"
         "|                          Enter string:                                | \n"
         "|____  _________________________________________________________________| \n"
         "     |/                                                                   \n");
+}
 
+void* KeyboardInput(void* str, int * error)
+{
+    StringPro* StrPro = (StringPro*) str;
+    
+    enteer();
     char keyBoardInputStr[100] = {0};
     scanf("%*c");
     scanf("%[^\n]",keyBoardInputStr);
-
-    if (keyBoardInputStr[0] == NULL)
+    m = scanf("%d", &n);
+    if (m == 1)
     {
-        *error = EMPTY_STRING;
-        return NULL;
+        scanf("%*c");
     }
 
     char * keyBoardInputPtr = malloc(sizeof(char)*strlen(keyBoardInputStr));
-    memcpy(keyBoardInputPtr,keyBoardInputStr,strlen(keyBoardInputStr)*sizeof(char));
+    my_memcpy(keyBoardInputPtr,keyBoardInputStr,strlen(keyBoardInputStr)*sizeof(char));
     StrPro->ptr = keyBoardInputPtr;
     StrPro->len = strlen(keyBoardInputPtr);
 }
@@ -42,19 +56,17 @@ void* KeyboardInput(void* str, int * error)
 void* concatenate(void* str,char * addStr)
 {
     StringPro* StrPro = (StringPro*) str;
+    StrPro->ptr = malloc(strlen(addStr));
 
-    if(StrPro->ptr != NULL)
-        StrPro->ptr = realloc(StrPro->ptr,(StrPro->len+strlen(addStr))*sizeof(char));
-
-    else
-        StrPro->ptr = malloc(strlen(addStr));
-
-    memcpy(StrPro->ptr + strlen(StrPro->ptr),addStr,strlen(addStr) + 1);
+    my_memcpy(StrPro->ptr + strlen(StrPro->ptr),addStr,strlen(addStr) + 1);
     StrPro->len+=strlen(addStr);
 }
 
 void* subString(void* str,int i, int j)
 {
+    char* pass = malloc(((j - i + 1) + 1)*sizeof(char));
+    char* pass_ptr = StrPro->ptr;
+    
     StringPro* StrPro =(StringPro*) str;
 
     if (j > StrPro->len)
@@ -62,10 +74,7 @@ void* subString(void* str,int i, int j)
 
     if (i < 0) i = 0;
 
-    char* pass = malloc(((j - i + 1) + 1)*sizeof(char));
-    char* pass_ptr = StrPro->ptr;
-
-    memcpy(pass,pass_ptr+i,(j - i + 1));
+    my_memcpy(pass,pass_ptr+i,(j - i + 1));
 
     StrPro = initialize(StrPro);
     StrPro->ptr = pass;
@@ -77,12 +86,7 @@ void* subString(void* str,int i, int j)
 void* SplitIntoWords(void*str, char* delim, int * error)
 {
     StringPro* StrPro = (StringPro*) str;
-    if (delim == NULL)
-    {
-        *error = NULL_DELIM;
-        return NULL;
-    }
-
+ 
     if(!StrPro->len)
     {
         printf(
@@ -94,24 +98,26 @@ void* SplitIntoWords(void*str, char* delim, int * error)
 
         return NULL ;
     }
-    char *pass_ptr = (char*) StrPro->ptr;
-    char* copy_ptr = malloc(sizeof(char) * (strlen(pass_ptr) + 1));
-    memcpy(copy_ptr, pass_ptr, (strlen(pass_ptr) + 1) * sizeof(char));
-    char* word = strtok(copy_ptr, delim );
+    char* word;
+    char *pass_ptr;
+    char* copy_ptr;
+    pass_ptr = (char*) StrPro->ptr;
+    copy_ptr = malloc(sizeof(char) * (strlen(pass_ptr) + 1));
+    my_memcpy(copy_ptr, pass_ptr, (strlen(pass_ptr) + 1) * sizeof(char));
+    word = strtok(copy_ptr, delim );
 
     StringPro** Word = malloc(sizeof(StringPro*));
 
     int i = 0;
     while(word != NULL)
     {
-        Word=realloc(Word, (i + 1) * sizeof(StringPro*));
+        Word = realloc(Word, (i + 1) * sizeof(StringPro*));
         Word[i] = initialize(str);
         Word[i]->ptr = malloc((strlen(word) + 1) * sizeof(char));
         Word[i]->len = strlen(word);
-        memcpy(Word[i]->ptr, word, strlen(word));
-        Word[i]->ProInfo->print(Word[i]);
+        my_memcpy(Word[i]->ptr, word, strlen(word));
+        Word[i]->ProInfo->print(Word[i]); i++;
         word = strtok(NULL, delim);
-        i++;
     }
 
 }
@@ -134,10 +140,8 @@ void* print(void* str) {
 void* freedom(void*str)
 {
     StringPro* StrPro = (StringPro*) str;
-
-    if(StrPro != NULL)
-        free(StrPro->ptr);
-
+    
+    free(StrPro->ptr);
     free(StrPro->ProInfo);
     free(StrPro);
 }
